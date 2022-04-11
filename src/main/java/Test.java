d6493a69b390807119a801698b629884a991d38f
ibrahim aygun <ssibrahimaygun@gmail.com>
2022-04-11 12:18:56 +0300

    ksş

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println("Thi is denemsse");
+        System.out.println("Thi is branch ibrahim");
     }
 
 }
